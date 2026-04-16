526316738cc2aa46e85542949fa1b526b84159ad
Eduardo-BGarcia <eduardobgarciax134abc39gmail.com>
2026-04-16 18:22:09 -0300

    test: implementado estrutura interna método e classe do service para processar a senha;

--- __test__/service/validadorSenha.test.js
+++ __test__/service/validadorSenha.test.js
@@ -1,3 +1,12 @@
-test("Deve validar senha com sucesso, rejeitando se menor que 8 caracteres", () => {
 
+
+test("Deve validar senha com sucesso, rejeitando se menor que 8 caracteres", () => {
+    // Arrange
+    const usuario = new Usuario({id: 1, nome: "João", senha: "1234567"});
+    
+    // Act 
+    const resultado = validadorSenha.validar(usuario.senha);
+    
+    // Assert
+    expect(resultado).toBe(false);
 });
---
+++ src/service/ValidadorSenhaService.js
@@ -0,0 +1,4 @@
+class ValidadorSenhaService {
+
+}
+module.exports = ValidadorSenhaService;
