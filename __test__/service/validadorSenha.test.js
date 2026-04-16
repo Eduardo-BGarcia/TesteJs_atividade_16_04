50799399ca8e92d7747f22b2213ea06fc7d7c31e
Eduardo-BGarcia <eduardobgarciax134abc39@gmail.com>
2026-04-16 20:40:29 -0300

    refactor: add estrutura "describe" para facilitar a execução em lote dos testes;

--- __test__/service/validadorSenha.test.js
+++ __test__/service/validadorSenha.test.js
@@ -1,68 +1,70 @@
 const Usuario = require("../../src/model/Usuario");
 const ValidadorSenhaService = require("../../src/service/ValidadorSenhaService");
 
-test("001 - Deve validar se a senha possui o minimo de 8 caracteres", () => {
-    // Arrange
-    const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*"});
-    
-    // Act 
-    const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
-    
-    // Assert
-    expect(resultado).toBe(true);
-});
+describe("Emprestimo", () => {
+    test("001 - Deve validar se a senha possui o minimo de 8 caracteres", () => {
+        // Arrange
+        const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
 
-test("002 - Deve validar se a senha possui letras maiúsculas", () => {
-    // Arrange
-    const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*"});
-    
-    // Act 
-    const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
-    
-    // Assert
-    expect(resultado).toBe(true);
-});
+        // Act 
+        const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
 
-test("003 - Deve validar se a senha possui letras minusculas", () => {
-    // Arrange
-    const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
+        // Assert
+        expect(resultado).toBe(true);
+    });
 
-    // Act 
-    const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
+    test("002 - Deve validar se a senha possui letras maiúsculas", () => {
+        // Arrange
+        const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
 
-    // Assert
-    expect(resultado).toBe(true);
-});
+        // Act 
+        const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
 
-test("004 - Deve validar se a senha possui numero", () => {
-    // Arrange
-    const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
+        // Assert
+        expect(resultado).toBe(true);
+    });
 
-    // Act 
-    const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
+    test("003 - Deve validar se a senha possui letras minusculas", () => {
+        // Arrange
+        const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
 
-    // Assert
-    expect(resultado).toBe(true);
-});
+        // Act 
+        const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
 
-test("005 - Deve validar se a senha possui caracter especial", () => {
-    // Arrange
-    const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
+        // Assert
+        expect(resultado).toBe(true);
+    });
 
-    // Act 
-    const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
+    test("004 - Deve validar se a senha possui numero", () => {
+        // Arrange
+        const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
 
-    // Assert
-    expect(resultado).toBe(true);
-});
+        // Act 
+        const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
 
-test("006 - Deve validar se a senha possui espaços em branco", () => {
-    // Arrange
-    const usuario = new Usuario({ id: 1, nome: "João", senha: "Edua rd0o! @#$%^& *" });
+        // Assert
+        expect(resultado).toBe(true);
+    });
 
-    // Act 
-    const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
+    test("005 - Deve validar se a senha possui caracter especial", () => {
+        // Arrange
+        const usuario = new Usuario({ id: 1, nome: "João", senha: "Eduard0o!@#$%^&*" });
 
-    // Assert
-    expect(resultado).toBe(false);
+        // Act 
+        const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
+
+        // Assert
+        expect(resultado).toBe(true);
+    });
+
+    test("006 - Deve validar se a senha possui espaços em branco", () => {
+        // Arrange
+        const usuario = new Usuario({ id: 1, nome: "João", senha: "Edua rd0o! @#$%^& *" });
+
+        // Act 
+        const resultado = ValidadorSenhaService.validadorSenha.validar(usuario.senha);
+
+        // Assert
+        expect(resultado).toBe(false);
+    });
 });
